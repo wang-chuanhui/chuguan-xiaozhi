b461017f5f6d2902e726cc56aa1684f17d5fe48f
wangchuanhui <372020909@qq.com>
2026-06-03 13:09:24 +0800

    volume

--- custom_components/chuguan-xiaozhi/number.py
+++ custom_components/chuguan-xiaozhi/number.py
@@ -2,7 +2,7 @@ from homeassistant.core import HomeAssistant, callback
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.components.number import NumberEntity, NumberDeviceClass
-from .chuguan.volume import watch_volume, get_volume, set_volume
+from .chuguan.volume import watch_volume, get_volume, set_volume, set_mute, get_audio_status
 import asyncio
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
@@ -25,6 +25,7 @@ class VolumeNumber(NumberEntity):
         self._attr_max_value = 100
         self._attr_step = 1
         self._volume = 100
+        self._muted = False
         self._monitor_process = None
 
     @property
@@ -33,17 +34,25 @@ class VolumeNumber(NumberEntity):
 
     async def async_set_native_value(self, value: float) -> None:
         """Update the current value."""
+        if self._muted:
+            await set_mute(False)
         await set_volume(int(value))
         self._volume = int(value)
 
     async def async_added_to_hass(self) -> None:
         """Entity added to hass."""
         await super().async_added_to_hass()
-        self._volume = await get_volume()
+        info = await get_audio_status()
+        self._muted = info["muted"]
+        if self._muted:
+            self._volume = 0
+        else:
+            self._volume = info["volume"]
         self.async_write_ha_state()
         @callback
         def handle_volume_change(info):
             mute = info["muted"]
+            self._muted = mute
             if mute:
                 self._volume = 0
             else:
