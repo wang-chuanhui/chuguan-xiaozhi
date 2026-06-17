0dacc24db34178c30e92780f87c95d8fe9f52efa
wangchuanhui <372020909@qq.com>
2026-06-17 17:51:28 +0800

    1

--- custom_components/chuguan-xiaozhi/light.py
+++ custom_components/chuguan-xiaozhi/light.py
@@ -78,6 +78,7 @@ class ScreenLight(LightEntity):
             return
         self._is_on = False
         self.schedule_update_ha_state()
+        self._reset_is_on(None)
         async_call_later(self.hass, 1, self._reset_is_on)
         raise NotImplementedError("请使用按键关闭屏幕")
 
