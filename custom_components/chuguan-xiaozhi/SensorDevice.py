b1a47332fb565545ac312b7ec1ff70d49b0ffacf
wangchuanhui <372020909@qq.com>
2026-07-07 17:07:05 +0800

    修复人体存在和运动感应默认触发的bug

--- custom_components/chuguan-xiaozhi/SensorDevice.py
+++ custom_components/chuguan-xiaozhi/SensorDevice.py
@@ -44,7 +44,7 @@ class MotionBinarySensor(BinarySensorEntity):
     
     async def async_added_to_hass(self):
         await super().async_added_to_hass()
-        self._is_on = await realDevice.getKV("motion_on")
+        self._is_on = await realDevice.getKV("motion_on") == '1'
         self.schedule_update_ha_state()
         # self.async_on_remove(
         #     async_track_time_interval(self.hass, self.update_is_on, timedelta(seconds=1))
@@ -78,7 +78,7 @@ class PresenceBinarySensor(BinarySensorEntity):
     
     async def async_added_to_hass(self):
         await super().async_added_to_hass()
-        self._is_on = await realDevice.getKV("presence_on")
+        self._is_on = await realDevice.getKV("presence_on") == '1'
         self.schedule_update_ha_state()
         # self.async_on_remove(
         #     async_track_time_interval(self.hass, self.update_is_on, timedelta(seconds=1))
